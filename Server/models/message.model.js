0733afbcc8f981a2b23b71f59ad54786b20fb158
Rajratnam02 <rajratnam2005@gmail.com>
2026-03-31 00:26:57 +0530

    Backend work started

---
+++ Server/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const messageSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    projectId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+      index: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Message", messageSchema);
---
+++ Server/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firebaseUid: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    photo: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User",userSchema);
