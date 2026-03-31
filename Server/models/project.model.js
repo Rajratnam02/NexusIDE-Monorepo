b40c5d57236339d069879a43bd80697e3d5112a0
Rajratnam02 <rajratnam2005@gmail.com>
2026-03-31 10:21:51 +0530

    Completed Project.model.js

--- Server/models/project.model.js
+++ Server/models/project.model.js
@@ -1,2 +1,61 @@
 import mongoose from "mongoose";
 
+const projectSchema = new mongoose.Schema(
+  {
+    roomId: {
+      type: String,
+      unique: true,
+      required: true,
+      index: true,
+    },
+    title: {
+      type: String,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        role: {
+          type: String,
+          enum: ["editor", "viewer"],
+          default: "viewer",
+        },
+      },
+    ],
+    files: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        content: {
+          type: String,
+        },
+        language: {
+          type: String,
+        },
+      },
+    ],
+    blockedUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lastEditedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Project", projectSchema);
